a10449a3c148140c60bb9704dddbbc66719a9b2c
Baden Miles <bami6878@colorado.edu>
2025-11-01 16:45:39 -0600

    added minimal base Express, Hbs, Bootstrap setup

---
+++ ProjectSourceCode/index.js
@@ -0,0 +1,36 @@
+const express = require('express'); // To build an application server or API
+const app = express();
+const handlebars = require('express-handlebars');
+const Handlebars = require('handlebars');
+const path = require('path');
+const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
+const bodyParser = require('body-parser');
+const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const bcrypt = require('bcryptjs'); //  To hash passwords
+const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
+
+
+//needed for express functionality
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//handlebars setup 
+const hbs = handlebars.create({
+  extname: 'hbs',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials',
+});
+
+//use hbs as view engine
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json()); 
+
+//Routes
+
+
+//startup
+app.listen(3000);
+console.log('Server is listening on port 3000');
