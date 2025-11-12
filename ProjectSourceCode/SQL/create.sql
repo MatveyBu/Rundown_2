f62dc7d1f339e297c68b3090313056ca93a784c8
Matvey Bubalo <bubalo.matvey@gmail.com>
2025-11-12 01:25:41 -0700

    added some sample data to the SQL

--- ProjectSourceCode/SQL/create.sql
+++ ProjectSourceCode/SQL/create.sql
@@ -34,7 +34,7 @@ CREATE TABLE communities(
     name VARCHAR(100) UNIQUE,
     description VARCHAR(500),
     community_type VARCHAR(60), --type is a non-reserved keyword but it's still best to avoid using it as an identifier
-    created_by INT,
+    created_by INT, --user_id of the creator
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     number_of_members INT DEFAULT 0 CHECK (number_of_members >= 0),
     FOREIGN KEY (created_by) REFERENCES users(user_id) ON DELETE SET NULL
@@ -66,3 +66,27 @@ CREATE TABLE verification_tokens(
     password VARCHAR(100) NOT NULL
 );
 
+
+--Default Sample Data
+--colleges data
+INSERT INTO colleges(college_name, location, domain)
+VALUES
+('University of Colorado Boulder','Boulder, CO','colorado.edu'),
+('University of Colorado Colorado Springs','Colorado Springs, CO','uccs.edu'),
+('Colorado State University','Fort Collins, CO','colostate.edu');
+--users data
+INSERT INTO users(first_name, last_name, username, password, email, role, college_id)
+VALUES
+('Matvey','Bubalo', 'MatveyBu', 'hashed_pw1','matvey.bubalo@colorado.edu','admin',1),
+('Liam','Clinton','licl','hashed_pw2','liam.clinton@uccs.edu','member',2),
+('Sofia','Reed','soree','hashed_pw3','sofia.reed@colostate.edu','moderator',3);
+--community data
+INSERT INTO communities(name, description,community_type,created_by, number_of_members)
+VALUES
+('Gaming Club','Community of students interested in video gaming', 'social', 1,1),
+('Sustainability Club','A place for students interested in sustainability to connect','social',2,0),
+('Homework Help','Join a community striving for academic success through collaboration!','academic',3,0);
+--users_communities connection test
+INSERT INTO users_communities(user_id, community_id)
+VALUES
+(1,1); --Matvey to the Gaming Club
